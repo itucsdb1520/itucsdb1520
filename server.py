075a5eb6f8e5982a52f709fb82a02832a8214353
ugursoy <ugursoy @itu.edu.tr>
2015-10-19 01:35:03 +0300

    Fixes

--- server.py
+++ server.py
@@ -8,9 +8,8 @@ from flask import render_template
 app = Flask(__name__)
 
 
-
-@app.route('/home')
 @app.route('/')
+@app.route('/home')
 def home():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
