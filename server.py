8eed1ef1e1d598c4ed9940b918abc2fd9c08b7c3
ugursoy <ugursoy @itu.edu.tr>
2015-10-19 01:26:10 +0300

    -Server.py edited so that other pages can open their corresponding htmls -HTML files and CSS files are created -Header section now emphasizes the current page -Added jquery to the project

--- server.py
+++ server.py
@@ -8,10 +8,37 @@ from flask import render_template
 app = Flask(__name__)
 
 
+
+@app.route('/home')
 @app.route('/')
 def home():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
+
+@app.route('/pilots')
+def pilots():
+    now = datetime.datetime.now()
+    return render_template('pilots.html', current_time=now.ctime())
+
+@app.route('/cars')
+def cars():
+    now = datetime.datetime.now()
+    return render_template('cars.html', current_time=now.ctime())
+
+@app.route('/tracks')
+def tracks():
+    now = datetime.datetime.now()
+    return render_template('tracks.html', current_time=now.ctime())
+
+@app.route('/brands')
+def brands():
+    now = datetime.datetime.now()
+    return render_template('brands.html', current_time=now.ctime())
+
+@app.route('/about')
+def about():
+    now = datetime.datetime.now()
+    return render_template('about.html', current_time=now.ctime())
 
 
 if __name__ == '__main__':
---
+++ static/js/header.js
@@ -0,0 +1,18 @@
+var link = window.location.href.split("/");
+
+$(document).ready(function(){
+    //We are at a page
+    var current_page = link[3];
+    var button_links = document.getElementsByClassName("button");
+    
+    for(var i=0; i<button_links.length; i++){
+      if (button_links[i].parentElement.href.split("/")[3] == current_page) {
+        if (current_page == " ") {
+          //homepage
+          $("a#home li").addClass("current-page");
+        }
+        $("a#" + current_page + " li").addClass("current-page");
+      }
+    }
+    
+  });
